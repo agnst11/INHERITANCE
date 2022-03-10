35c61dd3c78a87c5926d665a349edec90df2297e
agnst11 <99317673+agnst11@users.noreply.github.com>
2022-03-10 11:47:28 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,16 @@
+# Single Inheritance
+
+# Parent Class
+class Hewan:
+    def bersuara(self):
+        print('Kucing bersuara')
+
+# Child class mewarisi class Hewan
+class Kucing(Hewan):
+    def suara(self):
+        print('meong...meong...meong...')
+
+# Objek
+k=Kucing()
+k.suara()
+k.bersuara()
